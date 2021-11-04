2118b4d08bff654c644617771b3e207be27a1299
Ali Sanad <alisanad099@gmail.com>
2021-11-04 17:44:06 +0200

    final commit

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -18,9 +18,9 @@ const Pagination = ({postsPerPage, totalPosts, paginate}: PaginationProps) => {
       <ul className='pagination'>
         {pageNumbers.map((number) => (
           <li key={number} className='page-item'>
-            <a onClick={() => paginate(number)} href='!#' className='page-link'>
+            <button onClick={() => paginate(number)} className='page-link'>
               {number}
-            </a>
+            </button>
           </li>
         ))}
       </ul>
