f27e80d000b8fe4b1918419d411270f8bea8da14
Ali Sanad <alisanad099@gmail.com>
2021-11-04 16:04:36 +0200

    Updates

---
+++ precache-manifest.9aa49b03da8cbf3ac3e24329e6fc4ae2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d33a7975508b06dd1d701fd55ead6b27",
+    "url": "/simple-pagination/index.html"
+  },
+  {
+    "revision": "4dea63ec98f4a4958f92",
+    "url": "/simple-pagination/static/js/2.02cc656a.chunk.js"
+  },
+  {
+    "revision": "3413289604c9bdb42fcd",
+    "url": "/simple-pagination/static/js/main.d76eecfe.chunk.js"
+  },
+  {
+    "revision": "8e8210b2a464a7e8487e",
+    "url": "/simple-pagination/static/js/runtime~main.b82e6e78.js"
+  }
+]);
